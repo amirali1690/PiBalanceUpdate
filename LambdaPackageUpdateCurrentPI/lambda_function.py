aaa52a9760b597ff0885aae75fb03d60372c9888
unknown <>
2022-06-27 13:07:50 -0700

    fix pylint errors

--- LambdaPackageUpdateCurrentPI/lambda_function.py
+++ LambdaPackageUpdateCurrentPI/lambda_function.py
@@ -1,31 +1,30 @@
+"""
+lambda function to get pi cases balance from sqs and insert them to database.
+"""
 import os
-import pymysql
-import boto3
 import logging
 import ast
-import json
+import boto3
+import pymysql
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-
-
-
-
 def lambda_handler(event, context):
+    """
+    gets pi cases balance from sqs and insert them to database.
+    """
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
     logger.info('## EVENT')
-    logger.info(event)
-
-
+    logger.info(event,context)
     connection = pymysql.connect(host=os.getenv('RDS_HOST'),
                                 user = os.getenv('RDS_USER'),
                                 password = os.getenv('RDS_PASSWORD'),
                                 database = os.getenv('RDS_DB'),
                                 port = 3306,
                                 cursorclass= pymysql.cursors.DictCursor)
-
     cursor =connection.cursor()
-    client = boto3.client('sqs',region_name='us-west-2',endpoint_url='https://sqs.us-west-2.amazonaws.com')
+    client = boto3.client('sqs',region_name='us-west-2',
+                    endpoint_url='https://sqs.us-west-2.amazonaws.com')
     response = client.receive_message(
                         QueueUrl = 'https://sqs.us-west-2.amazonaws.com/849779278892/PiBalance',
                         AttributeNames=['All'],
@@ -36,19 +35,21 @@ def lambda_handler(event, context):
         for message in response['Messages']:
             print(message['Body'])
             caseInfo = ast.literal_eval(message['Body'])
-            id = caseInfo['caseId']
+            caseId = caseInfo['caseId']
             initialvisit = caseInfo['initialvisit']
             lastvisit = caseInfo['lastvisit']
             clinics = caseInfo['clinics']
             for clinic in clinics.keys():
                 if clinics[clinic] is None or clinics[clinic]=='None':
                     clinics[clinic]='0.00'
-                sql = 'UPDATE case_offices CO LEFT JOIN clinics CL ON CL.id=CO.office_id SET CO.balance =' + clinics[clinic] +  ' WHERE CO.case_id='+id+ ' AND CL.name="'+clinic+'"'
-                sql2 = 'UPDATE cases SET dateOfLastOfficeVisit="'+lastvisit+'", firstDayDate="'+initialvisit+'" WHERE id='+id
-                cursor.execute(sql,)
-                cursor.execute(sql2,)
+                sql = """UPDATE case_offices CO
+                         LEFT JOIN clinics CL ON CL.id=CO.office_id
+                         SET CO.balance = %s WHERE CO.case_id=%s AND CL.name=%s"""
+                sql2 = 'UPDATE cases SET dateOfLastOfficeVisit=%s, firstDayDate=%s WHERE id=%s'
+                cursor.execute(sql,(clinics[clinic],caseId,clinic))
+                cursor.execute(sql2,(lastvisit,initialvisit,caseId))
                 connection.commit()
-                delete = client.delete_message(
+                client.delete_message(
                     QueueUrl = 'https://sqs.us-west-2.amazonaws.com/849779278892/PiBalance',
                     ReceiptHandle= message['ReceiptHandle']
                 )
