cbb1a240c025f842c4da1a497fa349e1caad39cc
unknown <>
2022-07-08 10:29:41 -0700

    added space

--- LambdaPackageGetCurrentPI/lambda_function.py
+++ LambdaPackageGetCurrentPI/lambda_function.py
@@ -10,7 +10,6 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 def lambda_handler(event, context):
-    
     """
     gets current pi cases from database and send them to sqs.
     """
